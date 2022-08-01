0e5bbe6cef418f556888337f3ab44a5600362134
JosipTr <jtr.other13@gmail.com>
2022-08-01 15:40:30 +0200

    Write note states

--- lib/presentation/bloc/note_state.dart
+++ lib/presentation/bloc/note_state.dart
@@ -3,3 +3,11 @@ part of 'note_bloc.dart';
 abstract class NoteState {}
 
 class NoteInitial extends NoteState {}
+
+class NotesLoading extends NoteState {}
+
+class NotesLoaded extends NoteState {
+  final List<Note> notes;
+
+  NotesLoaded(this.notes);
+}
